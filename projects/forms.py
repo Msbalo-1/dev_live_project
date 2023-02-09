4e8ad1de609ebc7a7ac30fb0733e1c5e90a9fc9d
Msbalo-1 <mustaphabalogun860@gmail.com>
2023-02-09 16:17:57 +0100

    theme

--- projects/forms.py
+++ projects/forms.py
@@ -1,3 +1,4 @@
+import wtforms.widgets
 from django.forms import ModelForm
 from .models import Project
 
@@ -6,3 +7,15 @@ class ProjectForm(ModelForm):
         model = Project
         fields = '__all__'
         exclude = ['vote_total', 'vote_ratio']
+
+
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for key, value in self.fields.items():
+            value.widget.attrs.update({'class': 'input'})
+
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
+
