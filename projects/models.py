6d730858543580101027b9f77019cf28e961b11a
Msbalo-1 <mustaphabalogun860@gmail.com>
2023-02-10 20:50:15 +0100

    profiles

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 import uuid
-
+from users.models import Profile
 # Create your models here.
 class Project(models.Model):
-     # owner=
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True,)
     featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
@@ -22,6 +22,7 @@ class Project(models.Model):
     def image_url(self):
         try:
             img = self.featured_image.url
+
         except:
             img = ''
         return img
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=500, null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    shot_intro = models.CharField(max_length=300, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_images = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
+    social_Github = models.CharField(max_length=200, null=True, blank=True)
+    social_Twitter = models.CharField(max_length=200, null=True, blank=True)
+    social_YouTube = models.CharField(max_length=200, null=True, blank=True)
+    social_Linkdin = models.CharField(max_length=200, null=True, blank=True)
+    social_WebSite = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
