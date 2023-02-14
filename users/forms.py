d7984314ddd2584a271f43c681b4828bd5d00377
Msbalo-1 <mustaphabalogun860@gmail.com>
2023-02-14 18:43:11 +0100

    register

--- users/forms.py
+++ users/forms.py
@@ -7,3 +7,11 @@ class customUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
+
+
+
+    def __init__(self, *args, **kwargs):
+        super(customUserCreationForm, self).__init__(*args, **kwargs)
+
+        for key, value in self.fields.items():
+            value.widget.attrs.update({'class': 'input'})
