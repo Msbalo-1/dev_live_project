6e9284ccd6107dab76dc294e0a6ffe310ba89f63
Msbalo-1 <mustaphabalogun860@gmail.com>
2023-02-24 07:43:02 +0100

    api

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+
+# Create your views here.
+
+def getRoutes(request):
+
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+    ]
+
+    return JsonResponse(routes, safe=False)
